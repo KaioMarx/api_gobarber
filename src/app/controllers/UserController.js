71b11f4e95f84d4fcd454872217e02f88b8b37c9
KaioMarx <kaiomarx11@hotmail.com>
2019-11-20 11:54:17 -0300

    backup

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+import User from '../models/user';
+
+class UserController {
+  async store(req, res) {
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      return res.status(400).json({ error: 'Email already used !' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.status(200).json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
+routes.get('/users', UserController.store);
 routes.get('/', (req, res) => {
-  return res.json({ hello: 'world' })
-})
+  return res.status(200).json({ hello: 'api' });
+});
 
 export default routes;
